c80212545b47a6370c7fd9ad8196b1a8e71f179d
Ivan Chornyi <107806721+theIvanChornyi@users.noreply.github.com>
2022-10-18 18:43:25 +0300

    deleted theme provider

--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'normalize.css';
 import './index.css';
-import { theme } from 'theme';
-import { ThemeProvider } from 'styled-components';
+
 import { App } from 'components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
